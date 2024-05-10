a336628a9f91057aae1e4349445d73b3df922c79
Soroush Saberi <soroush.13830o@gmail.com>
2024-05-10 23:25:55 +0330

    tests done

--- src/main/java/sbu/cs/TaskScheduler.java
+++ src/main/java/sbu/cs/TaskScheduler.java
@@ -1,6 +1,8 @@
 package sbu.cs;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TaskScheduler
@@ -23,6 +25,12 @@ public class TaskScheduler
 
         @Override
         public void run() {
+            try {
+                Thread.sleep(processingTime);
+                System.out.println("Task of " + Thread.currentThread().getName() + " Done.");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             /*
             TODO
                 Simulate utilizing CPU by sleeping the thread for the specified processingTime
@@ -30,10 +38,27 @@ public class TaskScheduler
         }
     }
 
-    public static ArrayList<String> doTasks(ArrayList<Task> tasks)
-    {
+    public static ArrayList<String> doTasks(ArrayList<Task> tasks){
         ArrayList<String> finishedTasks = new ArrayList<>();
+        //Sorted by processing time of tasks
+        Collections.sort(tasks, new Comparator<Task>() {
+            @Override
+            public int compare(Task o1, Task o2) {
+                return Integer.compare(o1.processingTime, o2.processingTime);
+            }
+        });
+        Collections.reverse(tasks);
 
+        for(Task task : tasks){
+            Thread thread = new Thread(task , task.taskName);
+            thread.start();
+            finishedTasks.add(task.taskName);
+            try {
+                thread.join();
+            }catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+        }
         /*
         TODO
             Create a thread for each given task, And then start them based on which task has the highest priority
@@ -45,7 +70,15 @@ public class TaskScheduler
         return finishedTasks;
     }
 
-    public static void main(String[] args) {
-        // Test your code here
+    public static void main(String[] args){
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        tasks.add(new Task("A", 100));
+        tasks.add(new Task("B", 150));
+        tasks.add(new Task("C", 200));
+        tasks.add(new Task("E", 130));
+        tasks.add(new Task("F", 300));
+        ArrayList<String> finishedTasks = doTasks(tasks);
+        System.out.println("Finished Tasks: " + finishedTasks);
     }
 }
